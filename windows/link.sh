67fc70ac4b38f78fa2b9aec8b247dde4ef9caa63
Travis Jacobs <traviswjacobs@gmail.com>
2017-11-09 19:02:46 +0100

    Remove windows and old vim dotfiles

--- windows/link.sh
+++
@@ -1,6 +0,0 @@
-# run this from within babun
-REPO="$W/Documents/Github/dotfiles/windows"
-cp ~/.tmux.conf $REPO
-cp ~/.vimrc $REPO
-cp ~/.zshrc $REPO
-cp $W/appdataalias/Roaming/bug.n/Config.ini $REPO
